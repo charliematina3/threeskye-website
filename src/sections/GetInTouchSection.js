3a05b92665dd74f5bb119feb9785215a86e6a785
charlie <charlie@3skye.com>
2023-06-20 07:54:58 +1200

    [main] New images added. Contat page tweak.

--- src/sections/GetInTouchSection.js
+++ src/sections/GetInTouchSection.js
@@ -9,8 +9,8 @@ class GetInTouchSection extends Component {
 		const { bg } = this.props;
 		return (
 			<FadeInSection fadeUp delay="3">	
-				<TextSection bg={bg} header="Get in touch" style={{ paddingTop: 100, paddingBottom: 100 }} className="text-lg-center">
-					Interested in our products please hit the contact button below or alternatively email us at <FocusText><a target="_blank" rel="noreferrer" href="mailto:info@3skye.com">info@3skye.com</a></FocusText>.
+				<TextSection bg={bg} style={{ paddingTop: 100, paddingBottom: 100 }} className="text-lg-center">
+					Interested in our products please hit the contact button below or alternatively email us at <FocusText><a className="colour-blue" target="_blank" rel="noreferrer" href="mailto:info@3skye.com">info@3skye.com</a></FocusText>.
 					<br />
 					<br />
 					<Button routeChangePath="/get-in-touch">Get in touch</Button>
--- src/sections/HowItWorks.js
+++ src/sections/HowItWorks.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 // import ScrollFadeSection from '../components/ScrollFadeSection';
-import TsContainer from '../components/TsContainer';
 import ResearchFeaturesMobile from './ResearchFeaturesMobile';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import SectionHeader from '../components/SectionHeader';
 import ResearchFeaturesDesktop from './ResearchFeaturesDesktop';
 class HowItWorks extends Component {
 	render() {
