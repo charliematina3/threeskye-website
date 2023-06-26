024981841f08baaea815bdd9391d126412133a68
charlie <charlie@3skye.com>
2023-06-27 07:41:35 +1200

    [main] Fix weird pop in issue

--- src/sections/HomepageResearch.js
+++ src/sections/HomepageResearch.js
@@ -14,63 +14,61 @@ const HomepageResearch = (props) => {
 	}
 
 	return (
-		<FadeInSection>
-			<TsContainer homepage>
-				<FlexWrapper gap={32} alignItems="center" className="d-none d-md-flex">
-					<img
-						onClick={() => changeRoute("/research-platform")}
-						className="hero-image-left cursor-pointer"
-						src="/images/2023-images/products/research-2023.png"
-						alt="crm"
-						width="40%"
-					/>
-					<FlexWrapper flexDirection="column" width="60%">
-						<FadeInSection delay="1" className="text-center">
-							<h2>3SKYE Research Platform</h2>
-						</FadeInSection>
-						<FadeInSection className="text-center">
-							<p className="sub-header">Institutional and retail equity research the easy way</p>
-						</FadeInSection>
-						<FadeInSection className="text-center">
-							<ArrowButton routeChangePath={"/research-platform"}>Learn More</ArrowButton>
-						</FadeInSection>
-					</FlexWrapper>
+		<TsContainer homepage>
+			<FlexWrapper gap={32} alignItems="center" className="d-none d-md-flex">
+				<img
+					onClick={() => changeRoute("/research-platform")}
+					className="hero-image-left cursor-pointer"
+					src="/images/2023-images/products/research-2023.png"
+					alt="crm"
+					width="40%"
+				/>
+				<FlexWrapper flexDirection="column" width="60%">
+					<FadeInSection delay="1" className="text-center">
+						<h2>3SKYE Research Platform</h2>
+					</FadeInSection>
+					<FadeInSection className="text-center">
+						<p className="sub-header">Institutional and retail equity research the easy way</p>
+					</FadeInSection>
+					<FadeInSection className="text-center">
+						<ArrowButton routeChangePath={"/research-platform"}>Learn More</ArrowButton>
+					</FadeInSection>
 				</FlexWrapper>
-				<div className="d-block d-md-none">
-					<Row className="mb-3">
-						<Col xs="12" className="px-0">
-							<FlexWrapper justifyContent="center">
-								<img
-									onClick={() => changeRoute("/research-platform")}
-									className="cursor-pointer"
-									src="/images/2023-images/products/research-grouped.png"
-									alt="crm"
-									width="80%"
-									style={{ margin: "0 auto" }}
-								/>
-							</FlexWrapper>
+			</FlexWrapper>
+			<div className="d-block d-md-none">
+				<Row className="mb-3">
+					<Col xs="12" className="px-0">
+						<FlexWrapper justifyContent="center">
+							<img
+								onClick={() => changeRoute("/research-platform")}
+								className="cursor-pointer"
+								src="/images/2023-images/products/research-grouped.png"
+								alt="crm"
+								width="80%"
+								style={{ margin: "0 auto" }}
+							/>
+						</FlexWrapper>
+					</Col>
+				</Row>
+				<Row>
+					<Col xs="12" className="px-0">
+						<Col xs="12">
+							<FadeInSection delay="1" className="text-center">
+								<h2>3SKYE Research Platform</h2>
+							</FadeInSection>
+							<FadeInSection className="text-center">
+								<p className="sub-header">Institutional and retail equity research the easy way</p>
+							</FadeInSection>
 						</Col>
-					</Row>
-					<Row>
-						<Col xs="12" className="px-0">
-							<Col xs="12">
-								<FadeInSection delay="1" className="text-center">
-									<h2>3SKYE Research Platform</h2>
-								</FadeInSection>
-								<FadeInSection className="text-center">
-									<p className="sub-header">Institutional and retail equity research the easy way</p>
-								</FadeInSection>
-							</Col>
-							<Col xs="12">
-								<FadeInSection className="text-center">
-									<ArrowButton routeChangePath={"/wealth-platform"}>Learn More</ArrowButton>
-								</FadeInSection>
-							</Col>
+						<Col xs="12">
+							<FadeInSection className="text-center">
+								<ArrowButton routeChangePath={"/wealth-platform"}>Learn More</ArrowButton>
+							</FadeInSection>
 						</Col>
-					</Row>
-				</div>
-			</TsContainer>
-		</FadeInSection>
+					</Col>
+				</Row>
+			</div>
+		</TsContainer>
 	);
 }
 
--- src/sections/HomepageTsx.js
+++ src/sections/HomepageTsx.js
@@ -14,63 +14,55 @@ const HomepageTsx = (props) => {
 	}
 
 	return (
-		<FadeInSection>
-			<TsContainer homepage>
-				<FlexWrapper gap={32} alignItems="center" className="d-none d-md-flex">
-					<FlexWrapper flexDirection="column" width="60%">
-						<FadeInSection delay="1" className="text-center">
-							<h2>3SKYE-X</h2>
-						</FadeInSection>
-						<FadeInSection className="text-center">
-							<p className="sub-header">A Platform for High Net Worth & Family Offices</p>
-						</FadeInSection>
-						<FadeInSection className="text-center">
-							<ArrowButton routeChangePath={"/research-platform"}>Learn More</ArrowButton>
-						</FadeInSection>
-					</FlexWrapper>
-					<img
-						onClick={() => changeRoute("/3skye-x")}
-						className="hero-image-right cursor-pointer"
-						src="/images/2023-images/products/3sx-grouped.png"
-						alt="crm"
-						width="40%"
-					/>
+		<TsContainer homepage>
+			<FlexWrapper gap={32} alignItems="center" className="d-none d-md-flex">
+				<FlexWrapper flexDirection="column" width="60%">
+					<FadeInSection delay="1" className="text-center">
+						<h2>3SKYE-X</h2>
+					</FadeInSection>
+					<FadeInSection className="text-center">
+						<p className="sub-header">A Platform for High Net Worth & Family Offices</p>
+					</FadeInSection>
+					<FadeInSection className="text-center">
+						<ArrowButton routeChangePath={"/research-platform"}>Learn More</ArrowButton>
+					</FadeInSection>
 				</FlexWrapper>
-				<div className="d-block d-md-none">
-					<Row className="mb-3">
-						<Col xs="12" className="px-0">
-							<FlexWrapper justifyContent="center">
-								<img
-									onClick={() => changeRoute("/3skye-x")}
-									className="cursor-pointer"
-									src="/images/2023-images/products/3sx-grouped.png"
-									alt="crm"
-									width="80%"
-									style={{ margin: "0 auto" }}
-								/>
-							</FlexWrapper>
+				<img onClick={() => changeRoute("/3skye-x")} className="hero-image-right cursor-pointer" src="/images/2023-images/products/3sx-grouped.png" alt="crm" width="40%" />
+			</FlexWrapper>
+			<div className="d-block d-md-none">
+				<Row className="mb-3">
+					<Col xs="12" className="px-0">
+						<FlexWrapper justifyContent="center">
+							<img
+								onClick={() => changeRoute("/3skye-x")}
+								className="cursor-pointer"
+								src="/images/2023-images/products/3sx-grouped.png"
+								alt="crm"
+								width="80%"
+								style={{ margin: "0 auto" }}
+							/>
+						</FlexWrapper>
+					</Col>
+				</Row>
+				<Row>
+					<Col xs="12" className="px-0">
+						<Col xs="12">
+							<FadeInSection delay="1" className="text-center">
+								<h2>3SKYE-X</h2>
+							</FadeInSection>
+							<FadeInSection className="text-center">
+								<p className="sub-header">A Platform for High Net Worth & Family Offices</p>
+							</FadeInSection>
 						</Col>
-					</Row>
-					<Row>
-						<Col xs="12" className="px-0">
-							<Col xs="12">
-								<FadeInSection delay="1" className="text-center">
-									<h2>3SKYE-X</h2>
-								</FadeInSection>
-								<FadeInSection className="text-center">
-									<p className="sub-header">A Platform for High Net Worth & Family Offices</p>
-								</FadeInSection>
-							</Col>
-							<Col xs="12">
-								<FadeInSection className="text-center">
-									<ArrowButton routeChangePath={"/wealth-platform"}>Learn More</ArrowButton>
-								</FadeInSection>
-							</Col>
+						<Col xs="12">
+							<FadeInSection className="text-center">
+								<ArrowButton routeChangePath={"/wealth-platform"}>Learn More</ArrowButton>
+							</FadeInSection>
 						</Col>
-					</Row>
-				</div>
-			</TsContainer>
-		</FadeInSection>
+					</Col>
+				</Row>
+			</div>
+		</TsContainer>
 	);
 }
 
