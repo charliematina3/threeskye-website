85c6912e994eb5aa2436b8153419c20c5e33a0da
charlie <charlie@3skye.com>
2023-07-05 09:54:44 +1200

    [main] link fix

--- src/sections/HomepageTsx.js
+++ src/sections/HomepageTsx.js
@@ -19,12 +19,12 @@ const HomepageTsx = (props) => {
 				<FlexWrapper flexDirection="column" width="60%">
 					<FadeInSection delay="1" className="text-center">
 						<h2>3SKYE-X</h2>
-					</FadeInSection>
+					</FadeInSection>	
 					<FadeInSection className="text-center">
 						<p className="sub-header">A Platform for High Net Worth & Family Offices</p>
 					</FadeInSection>
 					<FadeInSection className="text-center">
-						<ArrowButton routeChangePath={"/research-platform"}>Learn More</ArrowButton>
+						<ArrowButton routeChangePath={"/3skye-x"}>Learn More</ArrowButton>
 					</FadeInSection>
 				</FlexWrapper>
 				<img onClick={() => changeRoute("/3skye-x")} className="hero-image-right cursor-pointer" src="/images/2023-images/products/3sx-grouped.png" alt="crm" width="40%" />
