9f40c26a35ff3b056a77bbcf6db4780f0f6999a1
charlie <charlie@3skye.com>
2024-02-21 10:18:04 +1300

    [main] Text updates

--- src/sections/funds/FundContent.js
+++ src/sections/funds/FundContent.js
@@ -7,7 +7,7 @@ export const FundContent = [
 				into one place, one-click creation of any type of fund document, editing, reviewing and approval process through to publication & distribution.
 				<br />
 				<br />
-				Useful for Quarterly Fund updates and Monthly Fact Sheets; we can add value to your current processes by saving you time and ensuring accuracy.
+				Useful for Quarterly Fund Updates and Monthly Fact Sheets; we can add value to your current processes by saving you time and ensuring accuracy.
 			</>
 		),
 		image: "images/funds/fund-automate.png",
@@ -20,21 +20,21 @@ export const FundContent = [
 				image: "images/2023-images/tiles/automatic-data-collection.png",
 			},
 			{
-				name: "Compliance",
+				name: "Easy Document Recall",
 				description: (
 					<>
-						Benefit from bespoke approval pathways for Compliance sign-off.
+						Want to update a previously published document? For example the fees have changes and you need to update the Product Disclosure Statement. Recall the latest PDS, update what you need, and send for approval.
 						<br />
 						<br />
-						Our clever Version Control eliminates confusion over which working document is the latest version. No more emails flying around clogging your email inbox.
+						Why do it in Word when you can have it all in our STP tool?
 					</>
 				),
-				image: "images/2023-images/tiles/compliance.png",
+				image: "images/2023-images/tiles/Group 1909.png",
 			},
 			{
-				name: "Disclaimers",
-				description: "Disclosure statements automatically appended to each document for comprehensive compliance management.",
-				image: "images/2023-images/tiles/disclaimers.png",
+				name: "All Fund documents",
+				description: "Takes the effort out of Quarterly Fund Updates and Monthly Fund Fact Sheets. Excellent for Product Disclosure Statements, SIPOs, OMIs etc.",
+				image: "images/2023-images/tiles/large-note-support.png",
 			},
 		],
 	},
@@ -62,6 +62,37 @@ export const FundContent = [
 				description:
 					"Bespoke publication options can include your website, data vendors, wrap platforms, advisors.  Via email, or API… you name the destination and we can send it there.",
 				image: "images/2023-images/tiles/distribution.png",
+			},
+		],
+	},
+	{
+		name: "Compliance and Risk",
+		description:
+			"Compliance teams can rest assured with our integrated approval workflow, automatic inclusion of Disclosures and transparent audit records. All in the same system. Easy.",
+		image: "images/funds/fund-publishing.png",
+		ref: "publishing",
+		tiles: [
+			{
+				name: "Version Control",
+				description:
+					"The ability to manage multiple document versions, track changes, ensuring clarity and accuracy. Easy to find the latest version - say goodbye to clogging up your email inbox with various versions.",
+				image: "images/2023-images/tiles/version-control.png",
+			},
+			{
+				name: "Compliance",
+				description: (
+					<>
+						An approval workflow is built into the platform in accordance with your internal processes. <br /> Request authorisation with one click and receive
+						approval from nominated users with full tracking, version control, and clear auditing of the approval process.
+					</>
+				),
+				image: "images/2023-images/tiles/compliance.png",
+			},
+			{
+				name: "Disclaimers",
+				description:
+					"Automatically includes your company’s disclosure statements. These statements are automatically appended to each document for comprehensive compliance management.",
+				image: "images/2023-images/tiles/disclaimers.png",
 			},
 		],
 	},
